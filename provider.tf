566f1f297b7d95b3c5342db3919f9f93fd1c527c
jc0b <j@jc0b.computer>
2025-06-23 11:08:14 +0200

    Messing around

--- provider.tf
+++ provider.tf
@@ -1,5 +1,10 @@
 provider "okta" {
   org_name = "trial-8979392"
   base_url = "okta.com"
-  private_key = base64decode(var.okta_private_key_base64)
+  private_key = local_file.okta_pk.filename
+}
+
+resource "local_file" "okta_pk" {
+  content_base64  = var.okta_private_key_base64
+  filename = "${path.module}/pk.pem"
 }
