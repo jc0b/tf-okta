32c9b677f467cb2446a6320d7a978bc51f948735
jc0b <j@jc0b.computer>
2025-07-07 11:49:59 +0200

    Switch back to envvar

--- provider.tf
+++ provider.tf
@@ -1,10 +1,4 @@
 provider "okta" {
   org_name = "trial-8979392"
   base_url = "okta.com"
-  private_key = local_file.okta_pk.filename
-}
-
-resource "local_file" "okta_pk" {
-  content_base64  = var.okta_private_key_base64
-  filename = "${path.module}/pk.pem"
 }
--- variables.tf
+++
@@ -1,4 +0,0 @@
-variable "okta_private_key_base64" {
-  description = "Base64 encoded Private Key"
-  type        = string
-}
