836ab6072893ef7fab7275d096d52b06858c9ea1
Jacob Burley <j@jc0b.computer>
2025-09-24 21:49:45 +0200

    hardcode scopes

--- provider.tf
+++ provider.tf
@@ -2,7 +2,7 @@ provider "okta" {
   org_name       = "trial-8979392"
   base_url       = "okta.com"
   client_id      = var.okta_client_id
-  scopes         = var.okta_scopes
+  scopes         = ["okta.directories.groups.manage", "okta.directories.manage", "okta.orgs.manage", "okta.orgs.read", "okta.users.manage", "okta.users.read"]
   private_key    = var.okta_private_key
   private_key_id = var.okta_private_key_id
 }
