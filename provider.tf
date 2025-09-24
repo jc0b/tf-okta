c6ae94df7f7b0a4d5195444aedbc43f49fc2eaf5
Jacob Burley <j@jc0b.computer>
2025-09-24 21:47:48 +0200

    Added auth?

--- provider.tf
+++ provider.tf
@@ -1,4 +1,8 @@
 provider "okta" {
-  org_name = "trial-8979392"
-  base_url = "okta.com"
+  org_name       = "trial-8979392"
+  base_url       = "okta.com"
+  client_id      = var.okta_client_id
+  scopes         = var.okta_scopes
+  private_key    = var.okta_private_key
+  private_key_id = var.okta_private_key_id
 }
