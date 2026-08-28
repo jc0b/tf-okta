dea0273dddcd8c4ba1fc22bc37766f3867b503c8
Jacob Burley <j@jc0b.computer>
2026-08-28 22:17:23 +0200

    Enable SCIM

--- integrations.tf
+++ integrations.tf
@@ -2,6 +2,6 @@ module "app_integrations" {
   source = "./modules/app_integration"
 
   integration_name  = "my terraform test integration"
-  user_provisioning = false
+  user_provisioning = true
 
 }
